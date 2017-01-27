21beea04734ee5bb10e03a6fefa928d2734781f7
ansonhwo <ahwong19@gmail.com>
2017-01-26 16:31:39 -0800

    Added foundation for portfolio.

---
+++ blog.js
@@ -0,0 +1,18 @@
+const initialState = {
+
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+const store = Redux.createStore(reducer, initialState)
+
+const blog = () => {
+
+}
+
+store.subscribe(blog)
